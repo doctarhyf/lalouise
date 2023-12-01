a234cf74d48f3f7a1639a860af844a3eb62da342
Docta Rhyf <drrhyf@gmail.com>
2023-12-01 13:08:26 +0200

    carousel

--- src/comps/MyCarousel.jsx
+++ src/comps/MyCarousel.jsx
@@ -5,6 +5,23 @@ import { Carousel } from "react-responsive-carousel";
 
 export default function MyCarousel() {
   return (
+    <div className="  overflow-hidden">
+      <Carousel className="-mt-[120pt]" autoPlay>
+        {[1, 2, 3, 4].map((p, i) => (
+          <div className="lg:h-[380pt]   ">
+            <div className="">
+              <img className="   " src={`ph${p}.jpg`} />
+            </div>
+            <p className="legend">Pharmacy {p}</p>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
+
+/* export default function MyCarousel() {
+  return (
     <div className="w-[850pt]  object-center h-[300pt] mb-8 bg-red-500 border-red-500 overflow-hidden">
       <Carousel className="-mt-[120pt]" autoPlay>
         {[1, 2, 3, 4].map((p, i) => (
@@ -16,12 +33,4 @@ export default function MyCarousel() {
       </Carousel>
     </div>
   );
-}
-
-// Don't forget to include the css in your page
-
-// Using webpack or parcel with a style loader
-// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-// Using html tag:
-// <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
+} */
